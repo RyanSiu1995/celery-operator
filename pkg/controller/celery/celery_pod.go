d888b708223a96e790f067ad6a2918a686a6b480
Ryan SIU <ryan.siu@cloverhealth.com>
2020-01-21 18:12:53 +0800

    Define the generate function for broker and scheduler

---
+++ pkg/controller/celery/celery_pod.go
@@ -0,0 +1,90 @@
+package celery
+
+import (
+	celeryprojectv4 "github.com/RyanSiu1995/celery-operator/pkg/apis/celeryproject/v4"
+	corev1 "k8s.io/api/core/v1"
+)
+
+func generateScheduler(cr *celeryprojectv4.Celery, brokerString string) *corev1.Pod {
+	labels := map[string]string{
+		"celery-app": cr.Name,
+		"type":       "scheduler",
+	}
+	return &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      cr.Name + "-scheulder",
+			Namespace: cr.Namespace,
+			Labels:    labels,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:    "",
+					Image:   "",
+					Command: "celery",
+					Args: []string{
+						"beat",
+						"-A",
+						cr.Name,
+						"-b",
+						brokerString,
+					},
+				},
+			},
+		},
+	}
+}
+
+func generateBroker(cr *celeryprojectv4.Celery) (*corev1.Pod, *corev1.Service) {
+	if cr.Spec.Broker.Type == celeryprojectv4.ExternalBroker {
+		return nil, nil
+	}
+
+	labels := map[string]string{
+		"celery-app": cr.Name,
+		"type":       "broker",
+	}
+
+	service := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      cr.Name + "-service",
+			Namespace: cr.Namespace,
+			Labels:    labels,
+		},
+		Spec: corev1.ServiceSpec{
+			Type:     "ClusterIP",
+			Selector: labels,
+			Port: []corev1.ServicePort{
+				{
+					Name:       "redis-port",
+					Port:       6379,
+					TargetPort: 6379,
+				},
+			},
+		},
+	}
+
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      cr.Name + "-service",
+			Namespace: cr.Namespace,
+			Labels:    labels,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:  "redis",
+					Image: "redis:3.0.5",
+					Ports: []corev1.ContainerPort{
+						{
+							Name:          "redis",
+							ContainerPort: 6379,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	return pod, service
+}
