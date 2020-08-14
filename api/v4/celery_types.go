748aef3745f62ab83d095f3e70ab5136afc1f20e
Ryan SIU <ryan.siu@cloverhealth.com>
2020-08-14 20:51:37 +0800

    Add the logic back from the legacy version

--- api/v4/celery_types.go
+++ api/v4/celery_types.go
@@ -1,6 +1,4 @@
 /*
-
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -17,6 +15,7 @@ limitations under the License.
 package v4
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -25,17 +24,48 @@ import (
 
 // CelerySpec defines the desired state of Celery
 type CelerySpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	Broker  CeleryBroker   `json:"broker,omitempty"`
+	Workers []CeleryWorker `json:"workers,omitempty"`
+}
 
-	// Foo is an example field of Celery. Edit Celery_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+// CeleryBroker defines the property of broker
+type CeleryBroker struct {
+	// Type defines the type of broker
+	Type BrokerType `json:"type,omitempty"`
+	// BrokerAddress defines the broker address for external broker type
+	// If it is not `external` type, this item will be ignored
+	BrokerAddress string `json:"brokerAddress,omitempty"`
+}
+
+// BrokerType defines the type of broker
+type BrokerType string
+
+const (
+	// RedisBroker is to use a dynamic redis instead within cluster
+	RedisBroker BrokerType = "redis"
+	// ExternalBroker is to use an external broker with given string
+	ExternalBroker BrokerType = "external"
+)
+
+// CeleryWorker defines the behavior of workers
+type CeleryWorker struct {
+	// DesiredNumber defines the number of worker if autoscaling is disabled
+	DesiredNumber int `json:"desiredNumber,omitempty"`
+	// Autoscaling defines the existence of HPA in celery worker
+	Autoscaling bool `json:"autoscaling,omitempty"`
+	// Min defines the minimum of workers if autoscaling is enabled
+	Min int `json:"min,omitempty"`
+	// Max defines the maximum of workers if autoscaling is enabled
+	Max int `json:"max,omitempty"`
+	// Target Queues defines the target queues these workers will handle
+	TargetQueues []string `json:"targetQueues,omitempty"`
+	// Resource defines the resources specification for these workers
+	Resource corev1.ResourceRequirements `json:"resource,omitempty"`
 }
 
 // CeleryStatus defines the observed state of Celery
 type CeleryStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	BrokerAddress string `json:"brokerAddress,omitempty"`
 }
 
 // +kubebuilder:object:root=true
--- controllers/celery_controller.go
+++ controllers/celery_controller.go
@@ -18,13 +18,18 @@ package controllers
 
 import (
 	"context"
+	sysError "errors"
 
 	"github.com/go-logr/logr"
+	appv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
-	cachev4 "github.com/RyanSiu1995/celery-operator/api/v4"
+	celeryprojectv4 "github.com/RyanSiu1995/celery-operator/api/v4"
 )
 
 // CeleryReconciler reconciles a Celery object
@@ -39,15 +44,80 @@ type CeleryReconciler struct {
 
 func (r *CeleryReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	_ = context.Background()
-	_ = r.Log.WithValues("celery", req.NamespacedName)
+	reqLogger := r.Log.WithValues("celery", req.NamespacedName)
 
-	// your logic here
+	// Fetch the Celery instance
+	instance := &celeryprojectv4.Celery{}
+	err := r.Client.Get(context.TODO(), req.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return ctrl.Result{}, err
+	}
+
+	// Define a new Broker object
+	var brokerAddress string
+	if instance.Spec.Broker.Type == celeryprojectv4.ExternalBroker {
+		if &instance.Spec.Broker.BrokerAddress == nil {
+			return ctrl.Result{}, sysError.New("Broker address hasn't been set")
+		}
+		brokerAddress = instance.Spec.Broker.BrokerAddress
+	} else {
+		brokerDeployment, brokerService, address := generateBroker(instance)
+		// Set Celery instance as the owner and controller
+		if err := controllerutil.SetControllerReference(instance, brokerDeployment, r.Scheme); err != nil {
+			return ctrl.Result{}, err
+		}
+		if err := controllerutil.SetControllerReference(instance, brokerService, r.Scheme); err != nil {
+			return ctrl.Result{}, err
+		}
+		found := &appv1.Deployment{}
+		err = r.Client.Get(context.TODO(), types.NamespacedName{Name: brokerDeployment.Name, Namespace: brokerDeployment.Namespace}, found)
+		if err != nil && errors.IsNotFound(err) {
+			reqLogger.Info("Creating a new Broker deployment", "Deployment.Namespace", brokerDeployment.Namespace, "Deployment.Name", brokerDeployment.Name)
+			if err := r.Client.Create(context.TODO(), brokerDeployment); err != nil {
+				return ctrl.Result{}, err
+			}
+
+			reqLogger.Info("Creating a new Broker service", "Service.Namespace", brokerDeployment.Namespace, "Pod.Name", brokerDeployment.Name)
+			if err := r.Client.Create(context.TODO(), brokerService); err != nil {
+				return ctrl.Result{}, err
+			}
+		}
+		// TODO Check if the service has been created
+		brokerAddress = address
+	}
+	instance.Status.BrokerAddress = brokerAddress
+	err = r.Client.Status().Update(context.TODO(), instance)
+	if err != nil {
+		return ctrl.Result{}, sysError.New("Cannot update broker status")
+	}
+	reqLogger.Info("Broker information has been collected")
+
+	// Define a new Scheduler object
+	schedulerDeployment := generateScheduler(instance, brokerAddress)
+	if err := controllerutil.SetControllerReference(instance, schedulerDeployment, r.Scheme); err != nil {
+		return ctrl.Result{}, err
+	}
+	found := &appv1.Deployment{}
+	err = r.Client.Get(context.TODO(), types.NamespacedName{Name: schedulerDeployment.Name, Namespace: schedulerDeployment.Namespace}, found)
+	if err != nil && errors.IsNotFound(err) {
+		reqLogger.Info("Creating a new Scheduler deployment", "Deployment.Namespace", schedulerDeployment.Namespace, "Deployment.Name", schedulerDeployment.Name)
+		if err := r.Client.Create(context.TODO(), schedulerDeployment); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
 
 	return ctrl.Result{}, nil
 }
 
 func (r *CeleryReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&cachev4.Celery{}).
+		For(&celeryprojectv4.Celery{}).
 		Complete(r)
 }
